82364537e1101971a6b6104a07b0c39f6d769674
ParsaProg <parsashaabani3@gmail.com>
2025-12-04 15:31:43 +0330

    page.tsx component, or main Home of this porject changes and added, remove the pert component things

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-
-export default function Home(){
-  const {theme, setTheme} = useTheme();
-  return <div className="w-full">
-    <button onClick={() => {
-      theme === "light" ? setTheme("dark"): setTheme("light")
-    }} className="rounded-lg m-3 p-3 font-bold dark:text-white text-black dark:border-slate-700 border-slate-400 border">Change Theme</button>
-  </div>
+export default function Home() {
+  return <div className="w-full"></div>;
 }
