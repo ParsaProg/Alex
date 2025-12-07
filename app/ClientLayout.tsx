1c09ffc370c0ad869612156b8b03e7e99b36aa3d
ParsaProg <parsashaabani3@gmail.com>
2025-12-07 16:46:17 +0330

    add main menu and menu button component besied the top scroll button in clientLayout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { LanguageProvider } from "@/src/context/languageContext";
+import MenuButton from "@/src/components/ui/menu/MenuButton";
+import TopScrollButton from "@/src/components/ui/TopScrollButton";
+import { useLanguage } from "@/src/context/languageContext";
 
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <LanguageProvider>{children}</LanguageProvider>;
+  return (
+    <>
+      {children}
+      <div
+        className={`flex items-center justify-between mx-auto left-[50%] translate-x-[-50%] sm:w-[88%] w-[90%] fixed bottom-8`}
+      >
+        <div className="flex flex-col items-start">
+          <MenuButton />
+        </div>
+        <div className="flex flex-col items-start">
+          <TopScrollButton />
+        </div>
+      </div>
+    </>
+  );
 }
