e608e27eaad8798148cef563f34dbcd74bc85da4
ParsaProg <parsashaabani3@gmail.com>
2025-12-07 17:10:03 +0330

    fix the overflow of the hero-shadow on mobile

--- src/components/ui/HeroShadow.tsx
+++ src/components/ui/HeroShadow.tsx
@@ -1,5 +1,5 @@
 export default function HeroShadow() {
   return (
-    <div className="left-[50%] translate-x-[-50%] opacity-[0.3] -z-1 absolute top-16 md:w-[800px] w-[450px] h-[100px] rounded-full dark:bg-neutral-400 bg-neutral-600 blur-3xl"></div>
+    <div className="left-[50%] translate-x-[-50%] opacity-[0.3] -z-1 absolute top-16 md:w-[800px] w-[80vw] h-[100px] rounded-full dark:bg-neutral-400 bg-neutral-600 blur-3xl"></div>
   );
 }
