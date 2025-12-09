ccb4790b40edbee827205113fa5b11137634c4bc
ParsaProg <parsashaabani3@gmail.com>
2025-12-10 00:59:09 +0330

    Header.tsx > backgroud changes **opactiy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { Languages, Menu, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Header() {
   const [mount, setMount] = useState<boolean | undefined>(undefined);
@@ -34,28 +35,31 @@ export default function Header() {
           visible: { opacity: 1, top: 0 },
         }}
         transition={{ delay: 0.3 }}
-        className="dark:bg-[#08080845] bg-[#ffffff45] z-9999 backdrop-blur-xl flex fixed top-0 w-full py-5 border-b dark:border-b-neutral-900 border-b-slate-300 "
+        className="dark:bg-[#08080845] bg-[#ffffffa9] z-9999 backdrop-blur-sm flex fixed top-0 w-full py-5 border-b dark:border-b-neutral-900 border-b-slate-300 "
       >
         <div className="sm:w-[80%] w-[90%] h-full mx-auto flex items-center justify-between">
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={{
-              hidden: { opacity: 0, x: -200 },
-              visible: { opacity: 1, x: 0 },
-            }}
-            transition={{ delay: 0.6 }}
-            className="flex items-start flex-col"
-          >
-            <h1 className="sm:text-2xl text-transparent bg-clip-text bg-linear-to-l font-bold dark:from-white from-black dark:to-blue-600 to-blue-600 text-xl">
-              {lang === "en" ? "Rahim Saedi's Book" : "کتاب رحیم ساعدی"}
-            </h1>
-            <h5 className="sm:text-md text-sm dark:text-slate-300 font-thin text-slate-600">
-              {lang === "en"
-                ? "How to speak english fluently"
-                : "چطور انگلیسی را روان، صحبت کنیم"}
-            </h5>
-          </motion.div>
+          <Link href={"/"}>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: { opacity: 0, x: -200 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              transition={{ delay: 0.6 }}
+              className="flex items-start flex-col"
+            >
+              <h1 className="sm:text-2xl text-transparent bg-clip-text bg-linear-to-l font-bold dark:from-white from-black dark:to-blue-600 to-blue-600 text-xl">
+                {lang === "en" ? "Rahim Saedi's Book" : "کتاب رحیم ساعدی"}
+              </h1>
+              <h5 className="sm:text-md text-sm dark:text-slate-300 font-thin text-slate-600">
+                {lang === "en"
+                  ? "How to speak english fluently"
+                  : "چطور انگلیسی را روان، صحبت کنیم"}
+              </h5>
+            </motion.div>
+          </Link>
+
           <motion.div
             initial="hidden"
             animate="visible"
