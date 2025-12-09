42758e5e1cbef3eee93164a3f907e5d4599676a3
ParsaProg <parsashaabani3@gmail.com>
2025-12-10 00:59:25 +0330

    Menu changes the link go to

--- src/components/ui/menu/Menu.tsx
+++ src/components/ui/menu/Menu.tsx
@@ -5,6 +5,7 @@ import { AnimatePresence } from "framer-motion";
 import { BookOpenText, Code2, Github, Stars, Users2 } from "lucide-react";
 import { motion } from "framer-motion";
 import { useLanguage } from "@/src/context/languageContext";
+import Link from "next/link";
 interface MainMenuType {
   showMenu: boolean;
   setShowMenu: (value: boolean) => void;
@@ -27,63 +28,77 @@ export default function MainMenu({ showMenu, setShowMenu }: MainMenuType) {
             lang == "en" ? "left-0" : "right-0"
           } w-[250px] flex flex-col items-center p-1 rounded-lg dark:bg-[#18181B] bg-white border dark:border-neutral-700 border-neutral-300 bottom-16`}
         >
-          <div className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent">
+          <Link
+            onClick={() => setShowMenu(false)}
+            href={"/developer"}
+            className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent"
+          >
             <div className="flex items-center gap-x-2 dark:text-neutral-200 text-neutral-900">
               <div className="dark:text-[#FFD700] text-[#ff3300]">
                 <Code2 size={15} />
               </div>
-              <h3
-                className="text-sm font-bold"
-              >
+              <h3 className="text-sm font-bold">
                 {lang === "en" ? "About Developer" : "دربارۀ برنامه‌نویس"}
               </h3>
             </div>
             <OpenIcon />
-          </div>
-          <div className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent">
+          </Link>
+          <Link
+            onClick={() => setShowMenu(false)}
+            href={"/collector"}
+            className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent"
+          >
             <div className="flex items-center gap-x-2 dark:text-neutral-200 text-neutral-900">
               <div className="dark:text-[#b66ceb] text-[#8b00ee]">
                 <Stars size={15} />
               </div>
-              <h3
-                className="text-sm font-bold"
-              >
+              <h3 className="text-sm font-bold">
                 {lang === "en" ? "Collector Information" : "گردآورندۀ کتاب"}
               </h3>
             </div>
             <OpenIcon />
-          </div>
+          </Link>
           <span className="w-full h-[0.5px] bg-neutral-300 dark:bg-neutral-800 my-1.5" />
-          <div className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent">
+          <Link
+            target="_blank"
+            href={"https://github.com/parsaprog/Alex"}
+            className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent"
+          >
             <div className="flex items-center gap-x-2 dark:text-neutral-200 text-neutral-900">
               <Github size={15} />
-              <h3
-                className="text-sm"
-              >
+              <h3 className="text-sm">
                 {lang === "en" ? "Source Code" : "سورس کد"}
               </h3>
             </div>
             <OpenIcon />
-          </div>
-          <div className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent">
+          </Link>
+          <Link
+            onClick={() => setShowMenu(false)}
+            href={"/book?tab-index=contributors"}
+            className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent"
+          >
             <div className="flex items-center gap-x-2 dark:text-neutral-200 text-neutral-900">
               <Users2 size={15} />
-              <h3
-                className="text-sm"
-              >
+              <h3 className="text-sm">
                 {lang === "en" ? "Contributors" : "مشارکت کنندان"}
               </h3>
             </div>
             <OpenIcon />
-          </div>
+          </Link>
           <span className="w-full h-[0.5px] bg-neutral-300 dark:bg-neutral-800 my-1.5" />
-          <div className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent">
+          <Link
+            onClick={() => setShowMenu(false)}
+            href={"/book?tab-index=book-info"}
+            className="w-full flex items-center justify-between rounded-md p-3 transition-all cursor-pointer hover:dark:bg-neutral-800 hover:dark:border-neutral-600 border border-transparent"
+          >
             <div className="flex items-center gap-x-2 dark:text-neutral-200 text-neutral-900">
               <BookOpenText size={15} />
-              <h3  className="text-sm">{lang === "en"? "Book Information": "اطلاعات کامل کتاب"}</h3>
+              <h3 className="text-sm">
+                {lang === "en" ? "Book Information" : "اطلاعات کامل کتاب"}
+              </h3>
             </div>
             <OpenIcon />
-          </div>
+          </Link>
         </motion.div>
       )}
     </AnimatePresence>
